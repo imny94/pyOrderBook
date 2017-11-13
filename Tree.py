befed3eb7347b7233e52b668d6935b13aa4aea4a
NicYeow <nicholas_yeow@mymail.sutd.edu.sg>
2017-11-13 23:50:25 +0800

    Added placeholders for class and functions so that Main.py can run

---
+++ Tree.py
@@ -0,0 +1,23 @@
+
+
+class Tree():
+
+	def __init__(self):
+		pass
+
+	def add(self, node):
+		pass
+
+	def remove(self, node):
+		pass
+
+	def lookup(self, node):
+		pass
+
+	def update(self, node):
+		pass
+
+class Node():
+
+	def __init__(self, details):
+		pass
