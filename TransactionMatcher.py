0e4bcd12c3865a3eafbcd324dde05fc4b8d6f20b
NicYeow <nicholas_yeow@mymail.sutd.edu.sg>
2017-11-17 17:48:31 +0800

    Finished up Draft Transaction Matcher. Commented out updating of event list for testing

--- TransactionMatcher.py
+++ TransactionMatcher.py
@@ -17,15 +17,15 @@ class TransactionMatcher():
         print "Matching Transactions!"
         while 1:
             minAskPrice = self.askTree.getSmallestPrice()
-            maxiBidPrice = self.bidTree.getLargestPrice()
+            maxBidPrice = self.bidTree.getLargestPrice()
             
-            if maxiBidPrice < minAskPrice:
+            if maxBidPrice < minAskPrice:
                 time.sleep(1)
                 break
 
             # Transaction is a profitable one!
             numAskingShares = self.askTree.getNumSharesForPrice(minAskPrice)
-            numBiddingShares = self.bidTree.getNumSharesForPrice(maxiBidPrice)
+            numBiddingShares = self.bidTree.getNumSharesForPrice(maxBidPrice)
 
             # Everything can be sold
             if numAskingShares > numBiddingShares:
@@ -34,15 +34,29 @@ class TransactionMatcher():
                 # Get the order Queue in terms of the orderIndex only. Note that the orderQueue is a list of Sets, so that data is filtered here
                 askOrders = [i[0] for i in askNode.getOrderQueue()]
                 # Get the list of orderIndex that can be satisfied
-                bidOrders = self.bidTree.getSatisfiableOrders(maxiBidPrice, numBiddingShares)
+                bidOrders = self.bidTree.getSatisfiableOrders(maxBidPrice, numBiddingShares)
                 # Note that the last item in bidOrders is a set that contains the order index to update
                 indexToUpdate, newNumShares = bidOrders.pop(-1)
 
-                # Update the eventlist
-                self.eventList.
-                pass
+                # # Update the eventlist
+                # currentTime = time.time()
+                # self.eventList.successTrades(askOrders, minAskPrice, currentTime)
+                # self.eventList.successTrades(bidOrders, maxBidPrice, currentTime)
+                # self.eventList.tradeAmount(indexToUpdate, maxBidPrice, currentTime, newNumShares)
 
             # There are some remaining shares that remains to be sold
             else:
-                
-                pass
+                # Remove the price node from the bidTree as all the orders in that node has been served
+                bidNode = self.bidTree.remove(maxBidPrice)
+                # Get the order Queue in terms of the orderIndex only. Note that the orderQueue is a list of Sets, so that data is filtered here
+                bidOrders = [i[0] for i in bidNode.getOrderQueue()]
+                # Get the list of orderIndex that can be satisfied
+                askOrders = self.askTree.getSatisfiableOrders(minAskPrice, numAskingShares)
+                # Note that the last item in bidOrders is a set that contains the order index to update
+                indexToUpdate, newNumShares = askOrders.pop(-1)
+
+                # # Update the eventlist
+                # currentTime = time.time()
+                # self.eventList.successTrades(askOrders, minAskPrice, currentTime)
+                # self.eventList.successTrades(bidOrders, maxBidPrice, currentTime)
+                # self.eventList.tradeAmount(indexToUpdate, minAskPrice, currentTime, newNumShares)
